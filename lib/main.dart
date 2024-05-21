121327c4ab09d5bb81be8ec47e584a8083807f90
Lachlan <21lkidd@wakatipu.school.nz>
2024-05-22 11:26:12 +1200

    Modularised Code made easier to refactor and use for the future

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() async {
-  runApp( const MyApp() );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -10,32 +10,42 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          elevation: 2,
-          backgroundColor: Colors.white,
-          shadowColor: Colors.black,
-          toolbarHeight: 80,
-          leading: IconButton(
-              icon: const Icon(Icons.more_horiz),
-              onPressed: () {
-              },
-              color: Colors.black,
-            ),
-          actions: [
-            Padding(
-              padding: const EdgeInsets.only(right: 10.0),
-              child: ElevatedButton(
-                onPressed: () {
-                },
-                child: const Text(
-                  'sign out',
-                  style: TextStyle(color: Colors.black),
-                ),
-             ),
-            ),
-          ],
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: const Header(),
+    );
+  }
+}
+
+class Header extends StatelessWidget {
+  const Header({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 2,
+        backgroundColor: Colors.white,
+        shadowColor: Colors.black,
+        toolbarHeight: 80,
+        leading: IconButton(
+          icon: const Icon(Icons.more_horiz),
+          onPressed: () {},
+          color: Colors.black,
         ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 10.0),
+            child: ElevatedButton(
+              onPressed: () {},
+              child: const Text(
+                'sign out',
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
