02f1e6fd1d47b60b19b518ea0fbf5a710258cfcd
Lachlan <21lkidd@wakatipu.school.nz>
2024-05-21 15:40:41 +1200

    Added heading
    Created the heading for the website

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,27 @@
 import 'package:flutter/material.dart';
 
-import 'src/app.dart';
-import 'src/settings/settings_controller.dart';
-import 'src/settings/settings_service.dart';
+// import 'src/app.dart';
+// import 'src/settings/settings_controller.dart';
+// import 'src/settings/settings_service.dart';
 
 void main() async {
-  // Set up the SettingsController, which will glue user settings to multiple
-  // Flutter Widgets.
-  final settingsController = SettingsController(SettingsService());
+  runApp( const MyApp() );
+}
 
-  // Load the user's preferred theme while the splash screen is displayed.
-  // This prevents a sudden theme change when the app is first displayed.
-  await settingsController.loadSettings();
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-  // Run the app and pass in the SettingsController. The app listens to the
-  // SettingsController for changes, then passes it further down to the
-  // SettingsView.
-  runApp(MyApp(settingsController: settingsController));
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          elevation: 2,
+          backgroundColor: Colors.white,
+          shadowColor: Colors.black,
+          toolbarHeight: 80,
+        ),
+      ),
+    );
+  }
 }
