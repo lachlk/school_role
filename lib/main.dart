47f5fb936490dfd8711436ee174c5fd2c2fdb4b5
Lachlan <21lkidd@wakatipu.school.nz>
2024-05-23 13:48:38 +1200

    Added Sebmented Button To List Item

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() async {
   runApp(const MyApp());
@@ -40,13 +41,66 @@ class Header extends StatelessWidget {
             child: ElevatedButton(
               onPressed: () {},
               child: const Text(
-                'sign out',
+                'Sign Out',
                 style: TextStyle(color: Colors.black),
               ),
             ),
           ),
         ],
       ),
+      body: const StudentList(),
+    );
+  }
+}
+
+class StudentList extends StatelessWidget {
+  const StudentList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: const <Widget>[
+          ListTile(
+            title: Text('bob'),
+            trailing: PresenceSelector(),
+          )
+        ]
+      )
+    );
+  }
+}
+
+enum Presence { present, late, absent }
+
+class PresenceSelector extends StatefulWidget {
+  const PresenceSelector({super.key});
+
+  @override
+  State<PresenceSelector> createState() => _PresenceSelectorState();
+}
+
+class _PresenceSelectorState extends State<PresenceSelector> {
+  Presence presenceView = Presence.absent;
+
+  @override
+  Widget build(BuildContext context) {
+    return SegmentedButton<Presence>(
+      segments: const <ButtonSegment<Presence>>[
+        ButtonSegment<Presence>(
+          value: Presence.present,
+          label: Text('Present'),
+        ),
+        ButtonSegment<Presence>(
+          value: Presence.late,
+          label: Text('Late'),
+        ),
+        ButtonSegment<Presence>(
+          value: Presence.absent,
+          label: Text('Absent'),
+        ),
+      ], 
+      selected: <Presence>{presenceView},
     );
   }
 }
