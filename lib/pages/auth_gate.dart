40bfe70361a96c6a81fa90e557b7b78a56cd97ad
Lachlan <21lkidd@wakatipu.school.nz>
2024-06-19 11:12:57 +1200

    Changed the body after authgate to classes

--- lib/pages/auth_gate.dart
+++ lib/pages/auth_gate.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:school_role/pages/presence_page.dart';
+import 'package:school_role/pages/classes_page.dart';
 
 class AuthGate extends StatelessWidget {
   const AuthGate({super.key});
@@ -50,7 +50,7 @@ class AuthGate extends StatelessWidget {
           );
         }
 
-        return const StudentList();
+        return const ClassesList();
       },
     );
   }
