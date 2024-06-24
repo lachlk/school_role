38a6621073ec92d92bebcad46a118a7983ac5662
Lachlan <21lkidd@wakatipu.school.nz>
2024-06-24 14:19:43 +1200

    Created classroom tiles

--- lib/pages/classes_page.dart
+++ lib/pages/classes_page.dart
@@ -1,5 +1,6 @@
 import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
+import 'package:school_role/main.dart';
 
 class ClassesList extends StatelessWidget {
   const ClassesList({super.key});
@@ -13,19 +14,44 @@ class ClassesList extends StatelessWidget {
       '7CHE',
       '7ENG',
       '7MAC',
+      '6MAC',
+      '6CHE'
     ];
 
     return Scaffold(
       body: GridView.count(
-        crossAxisCount: 2,
+        crossAxisCount: MediaQuery.of(context).size.shortestSide < 600  ? 2:4,
         children: <Widget>[
           for (var classroom in classes)
-          Card.filled(
-            margin: const EdgeInsets.all(10),
-            elevation: 4,
-            surfaceTintColor: Theme.of(context).colorScheme.primary.harmonizeWith(Colors.white),
-            child: Text(classroom),
-          )],
+            Card.filled(
+              margin: const EdgeInsets.all(10),
+              elevation: 1,
+              surfaceTintColor: Theme.of(context)
+                  .colorScheme
+                  .primary
+                  .harmonizeWith(Colors.white),
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context, 
+                    MaterialPageRoute(builder: (context) => const SecondRoute())
+                  );
+                },
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: FittedBox(
+                      fit: BoxFit.fill,
+                      child: Icon(Icons.groups, color: Theme.of(context).colorScheme.outline),
+                    )),
+                    Center(child: Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: Text(classroom)),
+                    ),
+                  ],
+                ))
+            )
+        ],
       ),
     );
   }
