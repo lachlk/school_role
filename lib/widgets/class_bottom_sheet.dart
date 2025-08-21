dd64e30ebfaf9b52e4d61b875f806558adaa418e
Lachlan <21lkidd@wakatipu.school.nz>
2025-08-21 14:17:49 +1200

    Weekday selector feature

--- lib/widgets/class_bottom_sheet.dart
+++ lib/widgets/class_bottom_sheet.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:school_role/services/class_service.dart';
+import 'package:school_role/widgets/weekday_selector.dart';
 
 class ClassBottomSheet extends StatelessWidget {
   const ClassBottomSheet({
@@ -17,48 +18,69 @@ class ClassBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(16),
-      height: 200,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          TextField(
-            controller: controller,
-            decoration: InputDecoration(
-              labelText: 'Class Name',
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10),
+    Map<String, int?> selectedDays = {
+      "Mon": null,
+      "Tue": null,
+      "Wed": null,
+      "Thu": null,
+      "Fri": null,
+    };
+    return Padding(
+      padding: EdgeInsets.only(
+        bottom: MediaQuery.of(context).viewInsets.bottom
+      ),
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        child: ListView(
+          shrinkWrap: true,
+          children: <Widget>[
+            TextField(
+              controller: controller,
+              decoration: InputDecoration(
+                labelText: 'Class Name',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
               ),
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              TextButton(
-                child: const Text("Close"),
-                onPressed: () => Navigator.of(context).pop(),
-              ),
-              ValueListenableBuilder<TextEditingValue>(
-                valueListenable: controller,
-                builder: (context, value, child) {
-                  final isEnabled = value.text.isNotEmpty;
-                  return TextButton(
-                    onPressed: isEnabled
-                      ? () async {
-                          final name = controller.text.trim();
-                          await classService.addClass(name);
-                          onGetClasses();
-                          Navigator.of(context).pop(true);
-                        }
-                      : null,
-                    child: const Text("Submit"),
-                  );
-                },
-              ),
-            ],
-          ),
-        ],
+            WeekdaySelector(
+              initialSelection: selectedDays,
+              onSelectedChanged: (days) {
+                selectedDays = days;
+              },
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                TextButton(
+                  child: const Text("Close"),
+                  onPressed: () => Navigator.of(context).pop(),
+                ),
+                ValueListenableBuilder<TextEditingValue>(
+                  valueListenable: controller,
+                  builder: (context, value, child) {
+                    final isEnabled = value.text.isNotEmpty;
+                    return TextButton(
+                      onPressed: isEnabled
+                        ? () async {
+                            final name = controller.text.trim();
+                            final filteredDays = Map.fromEntries(
+                              selectedDays.entries.where((e) => e.value != null),
+                            );
+
+                            await classService.addClass(name, filteredDays);
+                            onGetClasses();
+                            Navigator.of(context).pop(true);
+                          }
+                        : null,
+                      child: const Text("Submit"),
+                    );
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
