4f4328a753deb624ba98d22f1c2dc93aa358bd6a
Lachlan <21lkidd@wakatipu.school.nz>
2025-09-03 20:14:09 +1200

    Created app bar comments

--- lib/widgets/custom_app_bar.dart
+++ lib/widgets/custom_app_bar.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:school_role/widgets/sign_out_button.dart';
 
+/// A custom `AppBar` widget with a specific design and functionality.
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
-  final List<Widget>? actions;
-  final String? title;
-  final VoidCallback? onBackTap;
-
   const CustomAppBar({
     super.key,
     this.actions,
@@ -13,11 +10,20 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
     this.onBackTap,
   });
 
+  /// An optional list of widgets to display as actions on the right side of the app bar.
+  /// `SignOutButton` is always appended to this list.
+  final List<Widget>? actions;
+  /// Optional title for the app bar.
+  final String? title;
+  /// An optional callback function to be executed when the back button is tapped.
+  final VoidCallback? onBackTap;
+
   @override
   Size get preferredSize => const Size.fromHeight(100);
 
   @override
   Widget build(BuildContext context) {
+    // Creates a new list of actions by combining the provided `actions`.
     final allActions = <Widget>[
       ...(actions ?? []),
       const Padding(padding: EdgeInsets.only(right: 10.0), child: SignOutButton())
@@ -34,6 +40,7 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
           bottomRight: Radius.circular(20),
         ),
       ),
+      // If `onBackTap` is provided an `IconButton` with a back arrow is shown.
       leading: onBackTap != null
           ? IconButton(
               icon: const Icon(Icons.arrow_back),
@@ -42,6 +49,7 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
             )
           : null,
       title: title != null ? Text(title!) : null,
+      // Sets the list of actions to the combined list.
       actions: allActions,
     );
   }
